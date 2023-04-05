fedbec5c4337302708208455f3cf0689b010b8d6
Dzenkinstone <kozlovdanil6@gmail.com>
2023-04-05 23:15:02 +0200

    changes

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-  res.json({ message: "Hello message" });
+  res.json({ message: "hello message" });
 });
 
 router.get("/:contactId", async (req, res, next) => {
